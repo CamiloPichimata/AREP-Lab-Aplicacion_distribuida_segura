5f1dc3738607a605e386700f36a4aeffcaa20ab4
Camilo Pichimata <camilo.pichimata@mail.escuelaing.edu.co>
2022-04-19 11:39:09 -0500

    Implementación cifrado de contraseña. Se crea la página home.html

--- src/main/java/co/edu/escuelaing/AppDistribuidaSegura/SecureSparkWeb.java
+++ src/main/java/co/edu/escuelaing/AppDistribuidaSegura/SecureSparkWeb.java
@@ -6,15 +6,15 @@ public class SecureSparkWeb {
  
 	public static void main(String[] args) {
 		// Inicialización de usuarios registrados
-		UserApp user1 = new UserApp("User1", "passwdu1");
-		UserApp user2 = new UserApp("User2", "passwdu2");
-		UserApp user3 = new UserApp("User3", "passwdu3");
+		UserApp user1 = new UserApp("User1", "de7d0966dd91649474ecd891568e59977165c1d80a745a2e62599ec810b8c551");
+		UserApp user2 = new UserApp("User2", "3a09990e3ab649f1c81c22d21c9a52f6f889632f41db9987e1d9c043626dd7d3");
+		UserApp user3 = new UserApp("User3", "056f0753b4fa50c639811bd7500b2f3f5af973911a9da8e640f98769afa645f7");
 		RegisteredUsers registeredUsers = new RegisteredUsers();
 		registeredUsers.AddNewUser(user1);
 		registeredUsers.AddNewUser(user2);
 		registeredUsers.AddNewUser(user3);
 		
-		// Inicializar servidor
+		// Inicialización del servidor
 		System.out.println("Inicializando Servidor...");
 		// Estabaleciendo una conexión segura
 		secure(getKeystore(), "123456", null, null);
@@ -34,7 +34,8 @@ public class SecureSparkWeb {
 			String uName = req.queryParams("uName");
 			String uPasswd = req.queryParams("uPasswd");
 			System.out.println("uName: " + uName + " - uPasswd: " + uPasswd);
-			return registeredUsers.IsRegistered(uName, uPasswd);
+			Boolean isRegistered = registeredUsers.IsRegistered(uName, uPasswd);
+			return isRegistered;
 		});
 	}
 	
